698e735be91e2c5aec589b9050fc8a11da655e87
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-22 17:47:58 +0100

    :tada: file: exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Redis Basics' module"""
+import redis
+import uuid
+
+
+class Cache:
+    """ Cache class """
+
+    def __init__(self) -> None:
+        """ Class Initializer """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str) -> str:
+        """ 
+        Stores the `data` in Redis 
+
+        Args:
+            data (str): Data to store
+
+        Returns:
+            str: UUID
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
